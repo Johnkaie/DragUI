b0cee98fad67288fea58eaa3404342e9e3f62b5a
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-05 23:07:04 +0530

    component marketplace completely inegrated

--- server/services/analyticsService.js
+++ server/services/analyticsService.js
@@ -4,51 +4,65 @@ import Asset from "../models/Asset.js";
 import User from "../models/User.js";
 
 export const getDashboardStats =
-async () => {
+  async () => {
 
-  const [
-    components,
-    projects,
-    users,
-    assets,
-    frontend,
-    backend
-  ] = await Promise.all([
-    Component.countDocuments(),
-    Project.countDocuments(),
-    User.countDocuments(),
-    Asset.countDocuments(),
-
-    Component.countDocuments({
-      type:"frontend"
-    }),
-
-    Component.countDocuments({
-      type:"backend"
-    })
-  ]);
-
-  const downloads =
-    await Component.aggregate([
-      {
-        $group:{
-          _id:null,
-          total:{
-            $sum:"$downloads"
+    const componentViews =
+      await ComponentAnalytics.aggregate([
+        {
+          $group: {
+            _id: null,
+            total: {
+              $sum: "$views"
+            }
           }
         }
-      }
+      ]);
+
+    const [
+      components,
+      projects,
+      users,
+      assets,
+      frontend,
+      backend
+    ] = await Promise.all([
+      Component.countDocuments(),
+      Project.countDocuments(),
+      User.countDocuments(),
+      Asset.countDocuments(),
+
+      Component.countDocuments({
+        type: "frontend"
+      }),
+
+      Component.countDocuments({
+        type: "backend"
+      })
     ]);
 
-  return {
-    components,
-    projects,
-    users,
-    assets,
-    frontend,
-    backend,
+    const downloads =
+      await Component.aggregate([
+        {
+          $group: {
+            _id: null,
+            total: {
+              $sum: "$downloads"
+            }
+          }
+        }
+      ]);
 
-    downloads:
-      downloads[0]?.total || 0
+    return {
+      components,
+      projects,
+      users,
+      views:
+        componentViews[0]?.total || 0,
+      assets,
+      frontend,
+      backend,
+
+      downloads:
+        downloads[0]?.total || 0
+    };
   };
-};
