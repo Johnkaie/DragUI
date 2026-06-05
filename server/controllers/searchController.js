d2bbbd30903a789c3b2c37cf7cb4a870598d791c
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-05 22:37:29 +0530

    dependency phace 8

---
+++ server/controllers/searchController.js
@@ -0,0 +1,44 @@
+import {
+  searchComponents,
+}
+from "../services/searchService.js";
+
+export const search =
+async (
+  req,
+  res
+) => {
+
+  try {
+
+    const {
+      q,
+      type,
+      category,
+    } = req.query;
+
+    const results =
+      await searchComponents(
+        q,
+        {
+          type,
+          category,
+        }
+      );
+
+    res.json({
+      success: true,
+      results,
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message:
+        error.message,
+    });
+
+  }
+
+};
